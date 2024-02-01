670a4d11f01f47e72c29132854e2b482641f8f71
HarshithDataScience <129328608+HarshithDataScience@users.noreply.github.com>
2024-02-01 00:10:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.dataframe(my_fruit_list)
 
+# Adding multi-select
+
+streamlit.multislect("Pick some fruits:",list(my_fruit_list.index))
+
 
 
 
